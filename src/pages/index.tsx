2cd7efc7003cf2388d3be3fa974d84c0b8ad8fdf
tolunayozdemir <tolunay.ozdemir.56@gmail.com>
2023-01-13 00:00:23 +0300

    prettier init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import { StaticImage } from "gatsby-plugin-image";
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <div className="grid grid-cols-1 sm:grid-cols-2 sm:min-h-screen">
+      <div className="grid grid-cols-1 sm:min-h-screen sm:grid-cols-2">
         <StaticImage src="../images/life-style.png" alt="Life Style Image" />
-        <div className="lg:p-30 md:p-16 sm:p-8">
+        <div className="sm:p-8 md:p-16 lg:p-30">
           <h4 className="font-title text-title font-bold">
             Everyday items, we have something to suit every occasion.
           </h4>
