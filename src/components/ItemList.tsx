bee42bf534634bd2d2e74d54e41af8b41e25ed73
tolunayozdemir <tolunay.ozdemir.56@gmail.com>
2023-01-13 13:03:44 +0300

    static items

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -3,27 +3,6 @@ import React, { useRef } from "react";
 import RadioButton from "./RadioButton";
 import SquareButton from "./SquareButton";
 
-const items = [
-  {
-    name: "365 Signature Hoodie",
-    price: "€33.95",
-    image: "/hoodie.png",
-    colors: ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
-  },
-  {
-    name: "Organic Skinny High Waist Jeans",
-    price: "€33.95",
-    image: "/jeans.png",
-    colors: ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
-  },
-  {
-    name: "Organic Skinny High Waist Jeans 2",
-    price: "€33.95",
-    image: "/jeans2.png",
-    colors: ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
-  },
-];
-
 function Colors({ colors }: { colors: string[] }) {
   const name = Math.random().toString();
   return (
@@ -46,24 +25,48 @@ export default function ItemList() {
         ref={ref}
         className="flex-r o scrolling ml-6 flex overflow-x-auto overflow-y-hidden scroll-smooth transition-all sm:ml-12 xl:ml-30"
       >
-        {items.map(({ name, image, price, colors }) => {
-          return (
-            <li key={name} className="mr-8 flex-none">
-              <img
-                className="h-72 w-56 object-cover hover:cursor-pointer hover:opacity-75"
-                src={image}
-                alt={name}
-              />
-              <div className="mt-3 text-small">
-                <p>{name}</p>
-                <div className="mt-1 flex items-center justify-between">
-                  <span className="font-semibold">{price}</span>
-                  <Colors colors={colors} />
-                </div>
-              </div>
-            </li>
-          );
-        })}
+        <li className="mr-8 flex-none">
+          <StaticImage
+            className="h-72 w-56 object-cover hover:cursor-pointer hover:opacity-75"
+            src="../images/hoodie.png"
+            alt="365 Signature Hoodie"
+          />
+          <div className="mt-3 text-small">
+            <p>365 Signature Hoodie</p>
+            <div className="mt-1 flex items-center justify-between">
+              <span className="font-semibold">€33.95</span>
+              <Colors colors={["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"]} />
+            </div>
+          </div>
+        </li>
+        <li className="mr-8 flex-none">
+          <StaticImage
+            className="h-72 w-56 object-cover hover:cursor-pointer hover:opacity-75"
+            src="../images/pants.png"
+            alt="Organic Skinny High Waist Pants"
+          />
+          <div className="mt-3 text-small">
+            <p>Organic Skinny High Waist Pants</p>
+            <div className="mt-1 flex items-center justify-between">
+              <span className="font-semibold">€33.95</span>
+              <Colors colors={["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"]} />
+            </div>
+          </div>
+        </li>
+        <li className="mr-8 flex-none">
+          <StaticImage
+            className="h-72 w-56 object-cover hover:cursor-pointer hover:opacity-75"
+            src="../images/jeans2.png"
+            alt="Organic Skinny High Waist Jeans"
+          />
+          <div className="mt-3 text-small">
+            <p>Organic Skinny High Waist Jeans</p>
+            <div className="mt-1 flex items-center justify-between">
+              <span className="font-semibold">€33.95</span>
+              <Colors colors={["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"]} />
+            </div>
+          </div>
+        </li>
       </ul>
       <SquareButton
         onClick={() => {
